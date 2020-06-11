8870871d54bcbb531e14f38024aa0fae1f180b6e
anuragjha <toanuragjha@gmail.com>
2020-06-11 03:39:33 -0700

    Added ProvableInfo to build Provable API call

--- contracts/ApiFactory.sol
+++ contracts/ApiFactory.sol
@@ -1,7 +1,91 @@
 pragma solidity ^0.6.0;
 
+contract ApiFactory {
+    event print(string _message);
+    
+    
+    // ApiInfo[] public ApiArrayStore;
+    mapping(string => ApiInfo) ApiStore;
+    uint apiStoreCount;
+
+    function create(address _owner, string memory _name, string memory _uri, uint _cost) public {
+        if (keccak256(abi.encodePacked((ApiStore[_name].name))) == keccak256(abi.encodePacked(("")))) {
+            ApiInfo api = new ApiInfo(_owner, _name, _uri, _cost);
+            api.setContractAddress(address(api));
+            api.setProvableAddress(address(new ProvableInfo()));
+            // ApiArrayStore.push(api);
+            ApiStore[_name] = api;
+            emit print("API added");
+        } else {
+            emit print("API NOT added");
+        }
+    }
+
+    function createAndSendEther(address _owner, string memory _name, string memory _uri, uint _cost)
+        public
+        payable
+    {
+        if (keccak256(abi.encodePacked((ApiStore[_name].name))) == keccak256(abi.encodePacked(("")))) {
+            ApiInfo api = (new ApiInfo).value(msg.value)(_owner, _name, _uri, _cost);
+            api.setContractAddress(address(api));
+            api.setProvableAddress(address(new ProvableInfo()));
+            // ApiArrayStore.push(api);
+            ApiStore[_name] = api;
+            emit print("API added");
+        } else {
+            emit print("API NOT added");
+            
+        }
+        
+    }
+
+    // function getApiArrStoreInfo(uint _index)
+    //     public
+    //     view
+    //     returns (address contractAddress, address owner, string memory _name, uint balance)
+    // {
+    //     ApiInfo api = ApiArrayStore[_index];
+
+    //     return (api.contractAddress(), api.owner(), api.name(), address(api).balance);
+    // }
+    
+        function getApiInfo(string memory _name)
+        public
+        view
+        returns (address, address, string memory, uint)
+    {
+        ApiInfo api = ApiStore[_name];
+
+        return (api.contractAddress(), api.owner(), api.name(), address(api).balance);
+    }
+    
+    // function runIndex(uint _index) public view  returns(string memory) {
+    // //   ApiInfo ac = ApiInfo(ApiInfoAddress);
+    // //     return ac.run();
+    //     return ApiArrayStore[_index].run();
+    // }
+    
+    // function runAddress(address _contractAddress) public pure  returns(string memory) {
+    //     ApiInfo ac = ApiInfo(_contractAddress);
+    //     return ac.run();
+    //     //return ApiArrayStore[_index].run();
+    // }
+    
+    // function run(string memory _name) public view returns(string memory) {
+    //     return ApiStore[_name].run();
+    // }
+    
+    function run(string memory _name) public view returns(string memory) {
+        ApiInfo ai = ApiStore[_name];
+        address pa = ai.provableAddress();
+        ProvableInfo pi = ProvableInfo(pa);
+        return pi.run();
+    }
+}
+
 contract ApiInfo {
     address public contractAddress;
+    address public provableAddress;
     
     address public owner;
     string public name;
@@ -19,65 +103,21 @@ contract ApiInfo {
         contractAddress = _address;
     }
     
+    function setProvableAddress(address _address) public {
+        provableAddress = _address;
+    }
+    
     function run() pure public returns(string memory) {
         return ("running uri ...");
     }
 }
 
-contract ApiFactory {
-    ApiInfo[] public ApiArrayStore;
-    mapping(string => ApiInfo) ApiStore;
-
-    function create(address _owner, string memory _name, string memory _uri, uint _cost) public {
-        ApiInfo api = new ApiInfo(_owner, _name, _uri, _cost);
-        api.setContractAddress(address(api));
-        ApiArrayStore.push(api);
-        ApiStore[_name] = api;
-    }
-
-    function createAndSendEther(address _owner, string memory _name, string memory _uri, uint _cost)
-        public
-        payable
-    {
-        ApiInfo api = (new ApiInfo).value(msg.value)(_owner, _name, _uri, _cost);
-        api.setContractAddress(address(api));
-        ApiArrayStore.push(api);
-        ApiStore[_name] = api;
-    }
-
-    function getApiArrStoreInfo(uint _index)
-        public
-        view
-        returns (address contractAddress, address owner, string memory _name, uint balance)
-    {
-        ApiInfo api = ApiArrayStore[_index];
-
-        return (api.contractAddress(), api.owner(), api.name(), address(api).balance);
-    }
+contract ProvableInfo {
     
-        function getApiStoreInfo(string memory _name)
-        public
-        view
-        returns (address, address, string memory, uint)
-    {
-        ApiInfo api = ApiStore[_name];
-
-        return (api.contractAddress(), api.owner(), api.name(), address(api).balance);
-    }
+    constructor() public {}
     
-    function runIndex(uint _index) public view  returns(string memory) {
-    //   ApiInfo ac = ApiInfo(ApiInfoAddress);
-    //     return ac.run();
-        return ApiArrayStore[_index].run();
-    }
-    
-    function runAddress(address _contractAddress) public pure  returns(string memory) {
-        ApiInfo ac = ApiInfo(_contractAddress);
-        return ac.run();
-        //return ApiArrayStore[_index].run();
-    }
-    
-    function run(string memory _name) public view returns(string memory) {
-        return ApiStore[_name].run();
+    function run() pure public returns(string memory) {
+        return ("running ProvableApi uri ...");
     }
 }
+
